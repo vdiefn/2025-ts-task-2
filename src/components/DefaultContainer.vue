75682245a73ede26114d6df0be277c31c1d82289
Ellen <ddhkxxhxx@hotmail.com>
2025-12-07 22:11:57 +0800

    feat: add default container

---
+++ src/components/DefaultContainer.vue
@@ -0,0 +1,24 @@
+<script lang="ts" setup>
+
+interface Props {
+  loading: boolean
+}
+
+const props = withDefaults(defineProps<Props>(), {
+  loading: false
+})
+</script>
+<template>
+  <div v-if="props.loading" class="default-container h-100 w-100 d-flex justify-content-center align-items-center" >
+    <div  class="spinner-border " role="status">
+      <span class="visually-hidden">Loading...</span>
+    </div>
+  </div>
+
+  <slot v-else></slot>
+</template>
+<style lang="scss" scoped>
+  .default-container {
+    min-height:300px
+  }
+</style>
